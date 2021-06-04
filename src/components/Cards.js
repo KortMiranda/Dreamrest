cc5ecb88d77fe1aae8cedf98f9e1d9243fbed6ec
kortmiranda <kortneymiranda@outlook.com>
2021-06-04 16:38:19 -0500

    python successfully connected with working data

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios'
 
 function Cards(props) {
+    const [cards, setCards] = useState([])
+
+    useEffect(() => {
+        function getCard() {
+            axios.get('http://localhost:8000/cards/')
+            .then(res => {
+                setCards(res.data)
+                console.log(res.data)
+            })
+            .catch(console.error)
+        }
+        getCard()
+    }, [])
     return (
         <div>
-          I'm a card  
+           {cards.map((card) => {
+               return(
+                   <div className="card-display-all">
+                       <ul>
+                        <li className="card-list">
+                            <h4>{card.title}</h4>
+                            <img src={card.img_url} alt={card.title}/>
+                            <p>{card.description}</p>
+                        </li>
+                        </ul>
+                   </div>
+                   
+                   
+               )
+           })}
         </div>
     );
 }
