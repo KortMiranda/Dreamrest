0e3b57b4d6a0cc65dbd98839b73fb506cfc48be2
kortmiranda <kortneymiranda@outlook.com>
2021-06-11 22:57:17 -0500

    updated header

--- src/views/auth/Login.js
+++ src/views/auth/Login.js
@@ -30,7 +30,10 @@ const Login = () => {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        "Access-Control-Allow-Headers": "*"
+        // "Access-Control-Allow-Headers": "*",
+        "Access-Control-Allow-Origin" : "*",
+        "Access-Control-Allow-Methods" : "GET,POST,PUT,DELETE,OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With"
       },
       body: JSON.stringify(user)
     })
