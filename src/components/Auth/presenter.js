5db9a1accb1fcf2e40beef01572422395e72751f
kortmiranda <kortneymiranda@outlook.com>
2021-08-31 14:16:56 -0500

    add Auth component

---
+++ src/components/Auth/presenter.js
@@ -0,0 +1,28 @@
+import React from 'react'
+// import PropTypes from "prop-types"
+import LoginForm from "../LoginForm"
+import SignupForm from "../SignupForm"
+
+const Auth = (props, context) => (
+    <main>
+        <div>
+            {props.action === "login" && <LoginForm />}
+            {props.action === "signup" && <SignupForm />}
+        </div>
+        <div>
+            {props.action === "login" && (
+                <p>Don't have an account
+                    <span onClick={props.handleChangeAction}>Sign up</span>
+                </p>
+            )}
+            {props.action === "signup" && (
+                <p>Have an account?
+                    <span onClick={props.handleChangeAction}>Log in
+                    </span>
+                </p>
+            )}
+        </div>
+    </main>
+)
+
+export default Auth
